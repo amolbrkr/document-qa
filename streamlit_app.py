89dbd40958a64c187d023ea0c6d0adf660f20cfe
Amol Borkar <amolborkar5@gmail.com>
2024-09-05 16:12:17 +0000

    minor change

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,6 @@ st.write(
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("OpenAI API Key", type="password")
 
 client = OpenAI(api_key=openai_api_key)
