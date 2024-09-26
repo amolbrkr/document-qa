881fe50f695959ab7c0d64538520506019787283
Amol Borkar <amolborkar5@gmail.com>
2024-09-26 11:47:49 -0400

    Lab5 changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ pages = [
     st.Page("lab1.py", title="Lab 1"),
     st.Page("lab2.py", title="Lab 2"),
     st.Page("lab3.py", title="Lab 3"),
-    st.Page("lab4.py", title="Lab 4", default=True),
+    st.Page("lab4.py", title="Lab 4"),
+    st.Page("lab4.py", title="Lab 5", default=True),
 ]
 
 pg = st.navigation(pages)
