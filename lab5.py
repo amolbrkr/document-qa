651fe44198c99d3bf6c464eb2c8291c4ec15eac6
Amol Borkar <amolborkar5@gmail.com>
2024-09-26 11:55:56 -0400

    error fixes

--- lab5.py
+++ lab5.py
@@ -14,8 +14,6 @@ def get_current_weather(location):
     
     response = requests.get(url)
     data = response.json()
-    
-    # Extract temperatures and convert from Kelvin to Celsius
     temp = data['main']['temp'] - 273.15
     feels_like = data['main']['feels_like'] - 273.15
     temp_min = data['main']['temp_min'] - 273.15
@@ -26,11 +24,11 @@ def get_current_weather(location):
     return {
         "location": location,
         "temperature": round(temp, 2),
-        "feels_like": round(feels_like, 2),
         "temp_min": round(temp_min, 2),
         "temp_max": round(temp_max, 2),
-        "humidity": round(humidity, 2),
-        "weather_description": weather_description
+        "weather_description": weather_description,
+        "feels_like": round(feels_like, 2),
+        "humidity": round(humidity, 2)
     }
 
 def get_suggestion(weather_data):
--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ pages = [
     st.Page("lab2.py", title="Lab 2"),
     st.Page("lab3.py", title="Lab 3"),
     st.Page("lab4.py", title="Lab 4"),
-    st.Page("lab4.py", title="Lab 5", default=True),
+    st.Page("lab5.py", title="Lab 5", default=True),
 ]
 
 pg = st.navigation(pages)
